73508117558d70ae10bbe82705eb3b8e9f098b1b
IwashitaKazuma <iwashita.kazuma.bt@mynavi.jp>
2024-06-18 08:13:45 +0000

    6/18

---
+++ flask_app/templates/customer/customer_mypage.py
@@ -0,0 +1,38 @@
+from flask import redirect, render_template, session
+from flask_app.__init__ import app
+#ここにログインチェック関数のインポート
+
+
+# マイページメニュー（トップページ）
+@app.route("/mypage_mypage_top")
+def mypage_mypage_top():
+    if session["logged_in_customer"] == True:
+        return render_template("/mypage/mypage_top.html")
+    else:
+        return redirect("/customer/auth/login.html")
+    
+#アカウント情報に遷移
+@app.route("/mypage_manage_account")
+def mypage_manage_account():
+    if session["logged_in_customer"] == True:
+        return render_template("/customer/mypage/manage_account/info.html")
+    else:
+        return redirect("/customer/auth/login.html")
+    
+#予約一覧に遷移
+@app.route("/mypage_manage_ticket")
+def mypage_manage_ticket():
+    if session["logged_in_customer"] == True:
+        return render_template("/customer/mypage/manage_ticket/list.html")
+    else:
+        return redirect("/customer/auth/login.html")
+
+#退会に遷移
+@app.route("/mypage_manage_unsubscribe")
+def mypage_manage_unsubscribe():
+    if session["logged_in_customer"] == True:
+        return render_template("/customer/mypage/manage_unsubscribe/confirm.html")
+    else:
+        return redirect("/customer/auth/login.html")
+    
+
