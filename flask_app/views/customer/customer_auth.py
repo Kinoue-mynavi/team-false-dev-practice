1205a658d4495064cf0503d37b3ad30579673560
amanotaichi <amano.taichi.ix@mynavi.jp>
2024-06-18 08:12:57 +0000

    add signup customer_auth

--- flask_app/views/customer/customer_auth.py
+++ flask_app/views/customer/customer_auth.py
@@ -1,22 +1,25 @@
 from flask import render_template
 from flask_app.__init__ import app
 from flask import Flask, request
+from flask_app.models.functions.customer import create_customer_script
 
 # app = Flask(__name__)
 
+# 新規会員登録画面へ遷移
 @app.route('/new_member', methods=["GET", "POST"])
 def new_member():
     return render_template("/customer/auth/signup.html")
 
+# ログイン画面へ遷移
 @app.route('/login', methods=["GET", "POST"])
 def login():
     return render_template("/customer/auth/login.html")
 
-
-
-@app.route('/login', methods=['POST'])
+# 新規会員登録 情報入力画面
+@app.route('/customer_new_member', methods=['POST'])
 def customer_new_member():
     # フォームから送信されたデータを受け取る
+    # デバック用
     customer_account = request.form.get('customer_account')
     customer_password = request.form.get('customer_password')
     customer_name = request.form.get('customer_name')
@@ -25,6 +28,14 @@ def customer_new_member():
     customer_phone = request.form.get('customer_phone')
     customer_payment = request.form.get('customer_payment')
 
-    print(customer_account)
+    request_list = {"customer_account":customer_account, "customer_password":customer_password,
+            "customer_name":customer_name, "customer_zipcode":customer_zipcode,
+            "customer_address":customer_address, "customer_phone":customer_phone,
+            "customer_payment":customer_payment}
+    request_lists = [request_list]
 
-    # return render_template("customer/auth/login.html")
+    # データベースに追加
+    create_customer_script(request_lists)
+
+    # ログイン画面に遷移
+    return render_template("customer/auth/login.html")
