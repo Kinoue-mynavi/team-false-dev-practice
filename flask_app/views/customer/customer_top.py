82cbf407a444fa79e084d0a67660eb90a21961b7
RyosukeSasaki777 <sasaki.ryosuke.bp@mynavi.jp>
2024-06-18 08:17:51 +0000

    Fixed event top

--- flask_app/views/customer/customer_top.py
+++ flask_app/views/customer/customer_top.py
@@ -1,6 +1,19 @@
 from flask import render_template
 from flask_app.__init__ import app
+from flask_app.messages import ErrorMessages, InfoMessages
+from flask_app.models.functions. import
+
+
+# エラーメッセージクラスのインスタンス作成
+errorMessages = ErrorMessages()
+# インフォメーションメッセージクラスのインスタンス作成
+infoMessages = InfoMessages()
 
 @app.route("/customer_top")
 def customer_top():
+
+    # イベントカテゴリーが1件も取得できなければ、エラーメッセージ表示
+    if not mst_customer:
+        flash(errorMessages.w01('イベントカテゴリ'))
+
     return render_template("/customer/customer_top.html")
