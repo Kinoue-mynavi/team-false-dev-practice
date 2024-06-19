f30a1473deb81278e4f6204c82b1a8736babade1
amanotaichi <amano.taichi.ix@mynavi.jp>
2024-06-19 00:59:17 +0000

    update customer_auth

--- flask_app/views/customer/customer_auth.py
+++ flask_app/views/customer/customer_auth.py
@@ -28,6 +28,21 @@ def customer_new_member():
     customer_phone = request.form.get('customer_phone')
     customer_payment = request.form.get('customer_payment')
 
+    # バリデーションチェック
+    # 必須 アカウント名50文字以下 W2 W7
+    if 50 < len(customer_account):
+    # 必須 パスワード6ｰ10文字 W2 W8
+    if 6 >= len(customer_password) and 10 <= len(customer_password):
+    # 氏名20文字以下 W7
+    if 20 <= len(customer_name):
+    # 郵便番号7文字 W6 W10
+    if 7!= len(customer_zipcode):
+    # 住所50文字以下 W7
+    if 50 <= len(customer_address):
+    # 電話番号10-11文字 W8 W10
+    if 10 >= len(customer_phone) and 11 <= len(customer_phone):
+        
+    # 
     request_list = {"customer_account":customer_account, "customer_password":customer_password,
             "customer_name":customer_name, "customer_zipcode":customer_zipcode,
             "customer_address":customer_address, "customer_phone":customer_phone,
