95740272813af84bded3e07d17ad3d43204bdf03
RyosukeSasaki777 <sasaki.ryosuke.bp@mynavi.jp>
2024-06-19 05:18:47 +0000

    Update customer-top

--- flask_app/views/customer/customer_top.py
+++ flask_app/views/customer/customer_top.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 from flask_app.__init__ import app
 from flask_app.messages import ErrorMessages, InfoMessages
-from flask_app.models.functions. import
+from flask_app.models.functions.event import read_event
+from flask_app.models.mst_event import Mst_event
 
 
 # エラーメッセージクラスのインスタンス作成
@@ -11,9 +12,16 @@ infoMessages = InfoMessages()
 
 @app.route("/customer_top")
 def customer_top():
+    mst_event = read_event()
 
-    # イベントカテゴリーが1件も取得できなければ、エラーメッセージ表示
-    if not mst_customer:
+    # イベントが1件も取得できなければ、エラーメッセージ表示
+    if not mst_event:
         flash(errorMessages.w01('イベントカテゴリ'))
 
     return render_template("/customer/customer_top.html")
+
+@app.route("/event_reserv_confirm")
+def event_reserv_confirm():
+    flash("イベント詳細が開くはず、、")
+    return redirect(url_for("customer_top"))
+    # return render_template("/customer/event_detail.html")
