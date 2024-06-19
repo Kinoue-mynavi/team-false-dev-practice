3fd0fffe2ca436c82c6464e1424fb2f618b5a244
amanotaichi <amano.taichi.ix@mynavi.jp>
2024-06-19 07:41:48 +0000

    make hell

--- MySQL.session.sql
+++
@@ -1,1 +0,0 @@
-select * from mst_customer
--- flask_app/views/customer/customer_mypage.py
+++ flask_app/views/customer/customer_mypage.py
@@ -1,7 +1,9 @@
-from flask import render_template, redirect, session
+from flask import render_template, redirect, session, request
 from flask_app.__init__ import app
 from flask_app.messages import ErrorMessages, InfoMessages
-from flask_app.models.functions.ticket import read_ticket_event_id
+from flask_app.models.functions.ticket import read_ticket_event_id, read_ticket_one
+from flask_app.models.functions.customer import read_customer_one
+from flask_app.models.functions.event import read_event_one
 
 # エラーメッセージクラスのインスタンス作成
 errorMessages = ErrorMessages()
@@ -52,9 +54,43 @@ def display_confirmation():
 def confirm_cancel():
     if session["logged_in_customer"] == True:
         # チケット詳細を開いてキャンセル確認
-        
+        # ticket_id = request.form.get('ticket_id')
+        # デバック用
+        get_ticket_id = 1
 
-        return render_template("/customer/mypage/manage_ticket/confirm_cancel.html")
+        # チケット情報取得
+        ticket = read_ticket_one(get_ticket_id)
+        # チケットid, イベントid, 席種, 料金, 受付状態
+        print(ticket)
+
+        ticket_id = ticket[1].ticket_id
+        event_id = ["ticket.event_id"]
+        ticket_seat_id = ["ticket.ticket_seat_id"]
+        ticket_price = ["ticket.ticket_price"]
+        ticket_accept = ["ticket.ticket_accept"]
+
+        # 会員情報取得
+        customer_id = session["logged_in_customer_id"]
+        customer_info = read_customer_one(customer_id)
+        # 会員名, 郵便番号, 住所, 電話番号, 支払方法
+        customer_name = customer_info["customer_name"]
+        customer_zipcode = customer_info["customer_zipcode"]
+        customer_address = customer_info["customer_address"]
+        customer_phone = customer_info["customer_phone"]
+        customer_payment = customer_info["customer_info.payment"]
+
+        # イベント情報
+        event = read_event_one(event_id)
+        # イベント名, 開催日, 開催場所, イベント概要
+        event_name = event["event_name"]
+        event_days = event["event_days"]
+        event_place = event["event_place"]
+        event_overview = event["event_overview"]
+
+        return render_template("/customer/mypage/manage_ticket/confirm_cancel.html",
+            ticket_id=ticket_id, event_id=event_id, ticket_seat_id=ticket_seat_id, ticket_price=ticket_price, ticket_accept=ticket_accept,
+            customer_name=customer_name, customer_zipcode=customer_zipcode, customer_address=customer_address, customer_phone=customer_phone, customer_payment=customer_payment,
+            event_name=event_name, event_days=event_days, event_place=event_place, event_overview=event_overview)
     else:
         return redirect("/customer/auth/login.html")
 
