5b5d8fbe53c4f71dc02ed85af533d51b01547e6b
EC2 Default User <ec2-user@ip-172-31-42-14.ap-northeast-1.compute.internal>
2024-06-25 01:40:07 +0000

    トップページのエラーを修正

--- flask_app/views/customer/customer_top.py
+++ flask_app/views/customer/customer_top.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, flash, url_for, request
+from flask import render_template, flash, request
 from flask_app.__init__ import app
 from flask_app.messages import ErrorMessages, InfoMessages
 from flask_app.models.functions.event import read_event
@@ -13,21 +13,27 @@ infoMessages = InfoMessages()
 def customer_top():
     # 一覧のレコードを取得
     mst_events = read_event()
+
     # イベントが1件も取得できなければ、エラーメッセージ表示
     if not mst_events:
         flash(errorMessages.w01('イベントカテゴリ'))
-    else:
-        # ページネーション
-        ## 現在のページ番号を取得
-        page = int(request.args.get(get_page_parameter(), 1))
-        ## ページごとの表示件数
-        per_page = 10
-        ## ページネーションオブジェクトを作成
-        pagination = Pagination(page=page, per_page=per_page, total=len(mst_events))
-        # 表示するデータを取得
-        start = (page - 1) * per_page
-        end = start + per_page
-        displayed_events = mst_events[start:end]
+
+    # ページネーション
+    page = int(request.args.get(get_page_parameter(), 1))
+    ## ページごとの表示件数
+    per_page = 10
+    ## ページネーションオブジェクトを作成
+    pagination = Pagination(page=page, per_page=per_page, total=len(mst_events))
+    # 表示するデータを取得
+    start = (page - 1) * per_page
+    end = start + per_page
+
+    ## 現在のページ番号を取得
+    displayed_events = mst_events[start:end]
 
 
-    return render_template("/customer/customer_top.html", mst_events=displayed_events, pagination=pagination)
+    return render_template(
+        "/customer/customer_top.html",
+        mst_events=displayed_events,
+        pagination=pagination
+    )
